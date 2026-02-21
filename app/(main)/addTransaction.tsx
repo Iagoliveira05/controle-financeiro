1de441d65bd0079732fcc4b1d61d2db6e0e4119e
Iago <iago2011.andrade@hotmail.com>
2026-02-21 14:24:46 -0300

    Finalização UI tela de transação

--- app/(main)/addTransaction.tsx
+++ app/(main)/addTransaction.tsx
@@ -1,12 +1,27 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Touchable, TouchableOpacity, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { router } from 'expo-router';
 
 export default function addTransaction() {
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState('');
   const [type, setType] = useState<'income' | 'expense'>('income');
   
+  function handleSave() {
+    if (!title || !amount) {
+      Alert.alert("Erro",'Por favor, preencha todos os campos');
+      return;
+    }
+
+    console.log({
+      title,
+      amount: Number(amount),
+      type
+    });
+
+    router.back();
+  }
 
 
   return (
@@ -38,6 +53,42 @@ export default function addTransaction() {
             className='bg-zinc-800 p-4 rounded-2xl text-white mt-2'
           />
         </View>
+
+        <View className='flex-row justify-between mt-6 gap-6'>
+          <TouchableOpacity
+            onPress={() => setType('income')}
+            className={`flex-1 p-4 rounded-xl items-center ${
+              type === 'income'
+                ? 'bg-green-500'
+                : 'bg-zinc-800'
+            }`}
+          >
+            <Text className="text-white font-semibold text-lg">
+              Receita
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => setType('expense')}
+            className={`flex-1 p-4 rounded-xl items-center ${
+              type === 'expense'
+                ? 'bg-red-500'
+                : 'bg-zinc-800'
+            }`}
+          >
+            <Text className="text-white font-semibold text-lg">
+              Despesa
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleSave}
+          className='rounded-xl bg-green-600 p-4 items-center justify-center mt-10'
+        >
+          <Text className='text-2xl text-white font-bold'>Salvar</Text>
+        </TouchableOpacity>
+
       </View>
     </SafeAreaView>
   )
