afc1252a1a1b9798a033598505de5e1245abf21a
Iago <iago2011.andrade@hotmail.com>
2026-02-23 22:24:44 -0300

    Alteração segurança

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import Constants from "expo-constants";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const extra = Constants.expoConfig?.extra;
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBAELjRqjydreURX9kzlDTm0gf0au-0jH0",
-  authDomain: "controle-financie.firebaseapp.com",
-  projectId: "controle-financie",
-  storageBucket: "controle-financie.firebasestorage.app",
-  messagingSenderId: "192829543713",
-  appId: "1:192829543713:web:c42a279df84c2f9503da60"
+  apiKey: extra?.firebaseApiKey,
+  authDomain: extra?.firebaseAuthDomain,
+  projectId: extra?.firebaseProjectId,
+  storageBucket: extra?.firebaseStorageBucket,
+  messagingSenderId: extra?.firebaseMessagingSenderId,
+  appId: extra?.firebaseAppId
 };
 
 // Initialize Firebase
